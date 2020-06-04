04a71301b354a0c4fb1629b71b88fac44688997d
Mist <hdl730@163.com>
2020-06-04 16:39:52 +0800

    refactor test tools and add personal setting test

--- backend/ddl_killer/tests.py
+++ backend/ddl_killer/tests.py
@@ -8,6 +8,14 @@ from .views import *
 from unittest import mock
 
 
+class JsonTestClient(Client):
+    def post(self, *args, content_type='application/json', **kwargs):
+        return super().post(*args, content_type=content_type, **kwargs)
+
+    def get(self, *args, content_type='application/json', **kwargs):
+        return super().get(*args, content_type=content_type, **kwargs)
+
+
 class ViewTestCase(TestCase):
     TEST_USER_ID = '10000000'
     TEST_USER_NAME = 'TeSt_userName_123!@#$%^'
@@ -21,42 +29,50 @@ class ViewTestCase(TestCase):
         self._user_orm = None
 
     def setUp(self) -> None:
-        class JsonClient(Client):
-            def __init__(self, token_hook, **defaults):
-                super().__init__(**defaults)
-                self.token_hook = token_hook
 
-            def post(self, *args, content_type='application/json', **kwargs):
-                token = self.token_hook()
-                return super().post(*args, content_type=content_type, HTTP_AUTHORIZATION=token, **kwargs)
+        client = JsonTestClient()
+        self._client = client
 
-            def get(self, *args, content_type='application/json', **kwargs):
-                token = self.token_hook()
-                return super().get(*args, content_type=content_type, HTTP_AUTHORIZATION=token, **kwargs)
-
-        client = JsonClient(lambda: None if not self._user_data else self._user_data['token'])
-        self.client = client
-
-        def mock_mail_send(*args, **kwargs):
-            print('sending mock mail.. args:', args, kwargs)
-
-        with mock.patch('ddl_killer.utils.sendmail.YAG.send') as mail_obj:
-            mail_obj.side_effect = mock_mail_send
-            response = client.post('/api/register',
-                                   data={'uid': self.TEST_USER_ID,
-                                         'name': self.TEST_USER_NAME,
-                                         'password': self.password_encrypt,
-                                         'email': self.TEST_USER_EMAIL}).json()
+        response = self.post('/api/register',
+                             data={'uid': self.TEST_USER_ID,
+                                   'name': self.TEST_USER_NAME,
+                                   'password': self.password_encrypt,
+                                   'email': self.TEST_USER_EMAIL},
+                             auth_required=False)
         print('response:', response)
         user = User.objects.get(uid=self.TEST_USER_ID)
         user.is_active = True
         user.save()
         self._user_orm = user
 
+    @staticmethod
+    def mock_mail_send(*args, **kwargs):
+        print('sending mock mail.. args:', args, kwargs)
+
+    def _test_req_context(self, func, exp_code, auth_required):
+        def test_req_wrapper(*args, **kwargs):
+            token = None if not self._user_data else self._user_data['token']
+            with mock.patch('ddl_killer.utils.sendmail.YAG.send') as mail_obj:
+                mail_obj.side_effect = self.mock_mail_send
+                if auth_required:
+                    r_data = func(*args, HTTP_AUTHORIZATION=None, **kwargs).json()
+                    self.assertEqual(r_data['code'], 401, r_data)
+                r_data = func(*args, HTTP_AUTHORIZATION=token, **kwargs).json()
+                self.assertEqual(r_data['code'], exp_code)
+                return r_data
+
+        return test_req_wrapper
+
+    def post(self, *args, exp_code=200, auth_required=True, **kwargs):
+        return self._test_req_context(self._client.post, exp_code, auth_required)(*args, **kwargs)
+
+    def get(self, *args, exp_code=200, auth_required=True, **kwargs):
+        return self._test_req_context(self._client.get, exp_code, auth_required)(*args, **kwargs)
+
     @property
     def key_pair(self):
         if self._key_pair is None:
-            data = self.client.get('/api/security/pub-key').json()
+            data = self.get('/api/security/pub-key', auth_required=False)
             self._key_pair = SecurityKeyPair.objects.get(id=data['key_id'])
         return self._key_pair
 
@@ -72,46 +88,47 @@ class ViewTestCase(TestCase):
     def _login(self):
         if self._user_data is None:
             print('logging...')
-            r = self.client.post('/api/login', {'uid': self.TEST_USER_ID,
-                                                'password': self.password_encrypt})
-            self._user_data = r.json()
+            r = self.post('/api/login',
+                          {'uid': self.TEST_USER_ID,
+                           'password': self.password_encrypt},
+                          auth_required=False)
+            self._user_data = r
 
     def test_user_login_success(self):
         self._login()
         data = self._user_data
-        self.assertEqual(data['code'], 200)
         self.assertEqual(data['name'], self.TEST_USER_NAME)
 
     def test_user_login_not_found(self):
-        r = self.client.post('/api/login', {'uid': 'Not_exists',
-                                            'password': self.password_encrypt})
-        data = r.json()
-        self.assertEqual(data['code'], 404)
+        r = self.post('/api/login', {'uid': 'Not_exists',
+                                     'password': self.password_encrypt},
+                      exp_code=404,
+                      auth_required=False)
 
     def test_user_login_wrong_password(self):
-        r = self.client.post('/api/login', {'uid': self.TEST_USER_ID,
-                                            'password': 'wrong password'})
-        data = r.json()
-        self.assertEqual(data['code'], 401)
+        r = self.post('/api/login',
+                      {'uid': self.TEST_USER_ID,
+                       'password': 'wrong password'},
+                      exp_code=401)
 
     def test_user_login_not_activated(self):
         self._user_orm.is_active = False
         self._user_orm.save()
-        r = self.client.post('/api/login', {'uid': self.TEST_USER_ID,
-                                            'password': self.password_encrypt})
-        data = r.json()
-        self.assertEqual(data['code'], 400)
+        r = self.post('/api/login', {'uid': self.TEST_USER_ID,
+                                     'password': self.password_encrypt},
+                      exp_code=400,
+                      auth_required=False)
         self._user_orm.is_active = True
         self._user_orm.save()
 
     def test_edit_user(self):
         self._login()
-        data = self.client.post('/api/modify', {
+        data = self.post('/api/modify', {
             'uid': self.TEST_USER_ID,
             'name': self.TEST_USER_NAME,
             'password': '',
             'email': 'tmp_email@mail.com'
-        }).json()
+        })
         self.assertEqual(data['code'], 200)
         self.assertEqual(User.objects.get(uid=self.TEST_USER_ID).email,
                          'tmp_email@mail.com')
@@ -121,16 +138,16 @@ class ViewTestCase(TestCase):
 
     def test_logout(self):
         self._login()
-        r = self.client.post('/api/logout')
-        data = r.json()
+        r = self.post('/api/logout', auth_required=False)
+        data = r
         self.assertEqual(data['code'], 200)
 
         # token keeps available
 
     def test_show_user(self):
         self._login()
-        r = self.client.post('/api/user/{}/info'.format(self.TEST_USER_ID))
-        data = r.json()
+        r = self.post('/api/user/{}/info'.format(self.TEST_USER_ID))
+        data = r
         self.assertEqual(data['code'], 200)
         self.assertEqual(data['uid'], self.TEST_USER_ID)
         self.assertEqual(data['name'], self.TEST_USER_NAME)
@@ -138,14 +155,31 @@ class ViewTestCase(TestCase):
 
     def test_show_user_course(self):
         self._login()
-        r = self.client.post('/api/user/{}/courses'.format(self.TEST_USER_ID))
-        data = r.json()
+
+        UserCourse.objects.all().delete()
+
+        test_courses = [
+            Course.objects.create(name='testCourse' + str(i),
+                                  teacher='teacher' + str(i))
+            for i in range(10)
+        ]
+
+        for c in test_courses:
+            UserCourse.objects.create(user=self._user_orm, course=c)
+
+        r = self.post('/api/user/{}/courses'.format(self.TEST_USER_ID))
+        data = r
         self.assertEqual(data['code'], 200)
+        self.assertEqual(len(data['data']), len(test_courses))
+        course_data = data['data']
+        for data, course in zip(course_data, test_courses):
+            self.assertIn(data['cid'], {c.cid for c in test_courses})
+            self.assertIn(course.cid, {d['cid'] for d in course_data})
 
     def test_show_user_tasks(self):
         self._login()
-        r = self.client.post('/api/user/{}/tasks'.format(self.TEST_USER_ID))
-        data = r.json()
+        r = self.post('/api/user/{}/tasks'.format(self.TEST_USER_ID))
+        data = r
         self.assertEqual(data['code'], 200)
 
     def test_show_user_messages(self):
@@ -177,49 +211,58 @@ class ViewTestCase(TestCase):
                                              message=msg)
             unread_message_orm.append(orm)
 
-        r = self.client.get('/api/user/{}/message?type=read'.format(self.TEST_USER_ID))
-        data = r.json()
+        r = self.get('/api/user/{}/message?type=read'.format(self.TEST_USER_ID))
+        data = r
         self.assertEqual(data['code'], 200)
 
-        r = self.client.get('/api/user/{}/message?type=unread'.format(self.TEST_USER_ID))
-        data = r.json()
+        r = self.get('/api/user/{}/message?type=unread'.format(self.TEST_USER_ID))
+        data = r
         self.assertEqual(data['code'], 200)
 
-        r = self.client.get('/api/user/{}/message'.format(self.TEST_USER_ID))
-        data = r.json()
+        r = self.get('/api/user/{}/message'.format(self.TEST_USER_ID))
+        data = r
         self.assertEqual(data['code'], 200)
 
     def test_forget_password(self):
-        r = self.client.post('/api/user/forget/email',
-                             {'uid': self.TEST_USER_ID})
-        data = r.json()
+        r = self.post('/api/user/forget/email',
+                      {'uid': self.TEST_USER_ID},
+                      auth_required=False)
+        data = r
         self.assertEqual(data['code'], 200)
 
         record = PasswordModificationRecord.objects.get(user=self._user_orm)
         record.key_pair = self.key_pair  # hack for test
         record.save()
         verify_code = record.verify_code
-        r = self.client.post('/api/user/forget/verify',
-                             {
-                                 'uid': self.TEST_USER_ID,
-                                 'verify_code': verify_code
-                             })
-        data = r.json()
+        r = self.post('/api/user/forget/verify',
+                      {
+                          'uid': self.TEST_USER_ID,
+                          'verify_code': verify_code
+                      }, auth_required=False)
+        data = r
         self.assertEqual(data['code'], 200)
 
-        r = self.client.post('/api/user/forget/reset',
-                             {
-                                 'uid': self.TEST_USER_ID,
-                                 'password': self.password_encrypt,
-                             })
-        data = r.json()
+        r = self.post('/api/user/forget/reset',
+                      {
+                          'uid': self.TEST_USER_ID,
+                          'password': self.password_encrypt,
+                      }, auth_required=False)
+        data = r
         self.assertEqual(data['code'], 200)
 
     def test_report_bug(self):
         self._login()
-        r = self.client.post('/api/user/{}/report'.format(self.TEST_USER_ID),
-                             {'uid': self.TEST_USER_ID,
-                              'content': 'a bug'})
-        data = r.json()
-        self.assertEqual(data['code'], 200)
+        r = self.post('/api/user/{}/report'.format(self.TEST_USER_ID),
+                      {'uid': self.TEST_USER_ID,
+                       'content': 'a bug'})
 
+    def test_personal_setting(self):
+        self._login()
+        r = self.post('/api/user/{}/settings'.format(self.TEST_USER_ID),
+                      {'ddl_alert': True,
+                       'participate_alert': False,
+                       'resource_alert': True, })
+        r = self.get('/api/user/{}/settings'.format(self.TEST_USER_ID))
+        self.assertTrue(r['data'][0]['ddl_alert'])
+        self.assertFalse(r['data'][0]['participate_alert'])
+        self.assertTrue(r['data'][0]['resource_alert'])
